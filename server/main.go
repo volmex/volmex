d5779c2983844dbdb99eef2a90932e1c4c9654b1
Thomas Fankhauser <tommy@system8.io>
2017-07-04 15:20:32 +0200

    Fixed import path.

--- server/main.go
+++ server/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"github.com/docker/go-plugins-helpers/volume"
-	"gitlab.mi.hdm-stuttgart.de/volmex"
+	"gitlab.mi.hdm-stuttgart.de/fankhauser/volmex"
 )
 
 func main() {
-	d := &volmex.VolmexDriver{}
+	d := volmex.New()
 	h := volume.NewHandler(d)
 	err := h.ServeUnix("volmex", 0)
 	if err != nil {
