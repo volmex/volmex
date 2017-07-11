6e7576c84f7199bda97ae12d950ca5c472932cdb
Thomas Maier <contact@thomas-maier.net>
2017-07-11 17:42:35 +0200

    refactored state tests

--- state_test.go
+++ state_test.go
@@ -1,35 +1,39 @@
 package volmex
 
 import (
-	"github.com/docker/go-plugins-helpers/volume"
+	"fmt"
 	"testing"
+	"time"
+
+	"github.com/docker/go-plugins-helpers/volume"
 )
 
 func TestFileState(t *testing.T) {
-	s := NewFileState("/tmp/test.json")
+	fname := fmt.Sprintf("/tmp/test.json %v", time.Now().Unix())
 
+	// create a FileState, add a volume configuration and save the state
+	s := NewFileState(fname)
 	s.Put("foo", &Volume{
 		Volume: volume.Volume{
 			Name: "foo",
-		}})
-
+		},
+	})
 	err := s.Save()
-
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	ss := NewFileState("/tmp/test.json")
+	// create new FileState, load the state and check if our previously added configuration is still there
+	ss := NewFileState(fname)
 	err = ss.Load()
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	v, err := ss.Get("foo")
 	if err != nil {
 		t.Fatal(err)
 	}
 	if v.Volume.Name != "foo" {
-		t.Fatal("did not load volumes correctly")
+		t.Fatal("did not load volume configuration correctly")
 	}
 }
